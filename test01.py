470935f0320c8a8b0580cd6230d2dbe602237da2
AsaEwing <poiu60052@gmail.com>
2017-03-17 01:44:14 +0800

    001

--- test01.py
+++ test01.py
@@ -1,9 +1,22 @@
+from __future__ import print_function
+
+'''
+# TODO:1
+print("### Start 1")
+print("### End 1")
+print("### Problem 1")
+'''
+
+# TODO:..................................1
+print("# # # 1 # # #")
+
 # TODO:1
 print("### Start 1")
 # These are all the modules we'll be using later. Make sure you can import them
 # before proceeding further.
-from __future__ import print_function
-import matplotlib.pyplot as plt
+
+import matplotlib.pyplot as mpplt
+import matplotlib.image as mpimg
 import numpy as np
 import os
 import sys
@@ -12,7 +25,8 @@ from IPython.display import display, Image
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
-from six.moves import cPickle as pickle
+import six.moves.cPickle as pickle
+
 # from PIL.Image import core as _imaging
 
 # Config the matplotlib backend as plotting inline in IPython %matplotlib inline
@@ -95,6 +109,11 @@ train_folders = maybe_extract(train_filename)
 test_folders = maybe_extract(test_filename)
 print("### End 3")
 print("### Problem 1")
+# display(Image(filename="notMNIST_small/A/Q0NXaWxkV29yZHMtQm9sZEl0YWxpYy50dGY=.png"))
+mpplt.figure(1)
+mpplt.imshow(mpimg.imread("notMNIST_small/A/Q0NXaWxkV29yZHMtQm9sZEl0YWxpYy50dGY=.png"))
+# mpplt.show()
+print("@   @   @   P1")
 
 # TODO:4
 print("### Start 4")
@@ -146,6 +165,7 @@ def maybe_pickle(data_folders, min_num_images_per_class, force=False):
             try:
                 with open(set_filename, 'wb') as f:
                     pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+                    # pickle.HIGHEST_PROTOCOL
             except Exception as e:
                 print('Unable to save data to', set_filename, ':', e)
 
@@ -156,4 +176,137 @@ train_datasets = maybe_pickle(train_folders, 45000)
 test_datasets = maybe_pickle(test_folders, 1800)
 print("### End 4")
 print("### Problem 2 & 3")
+
+# Problem 2
+pickle_file = train_datasets[0]  # index 0 should be all As, 1 = all Bs, etc.
+with open(pickle_file, 'rb') as f:
+    print("@   @   @   P2")
+    letter_set = pickle.load(f)  # unpickle
+    sample_idx = np.random.randint(len(letter_set))  # pick a random image index
+    sample_image = letter_set[sample_idx, :, :]  # extract a 2D slice
+
+    # mpplt.imshow(letter_image)  # display it
+    mpplt.figure(2)
+    mpplt.imshow(sample_image)  # display it
+    # mpplt.show()
+    # raw_input()
+
+# Problem 3
+
+
+# TODO:5
+print("### Start 5")
+
+
+def make_arrays(nb_rows, img_size):
+    if nb_rows:
+        dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
+        labels = np.ndarray(nb_rows, dtype=np.int32)
+    else:
+        dataset, labels = None, None
+    return dataset, labels
+
+
+def merge_datasets(pickle_files, train_size, valid_size=0):
+    num_classes = len(pickle_files)
+    valid_dataset, valid_labels = make_arrays(valid_size, image_size)
+    train_dataset, train_labels = make_arrays(train_size, image_size)
+    vsize_per_class = valid_size // num_classes
+    tsize_per_class = train_size // num_classes
+
+    start_v, start_t = 0, 0
+    end_v, end_t = vsize_per_class, tsize_per_class
+    end_l = vsize_per_class + tsize_per_class
+    for label, pickle_file in enumerate(pickle_files):
+        try:
+            with open(pickle_file, 'rb') as f:
+                letter_set = pickle.load(f)
+                # let's shuffle the letters to have random validation and training set
+                np.random.shuffle(letter_set)
+                if valid_dataset is not None:
+                    valid_letter = letter_set[:vsize_per_class, :, :]
+                    valid_dataset[start_v:end_v, :, :] = valid_letter
+                    valid_labels[start_v:end_v] = label
+                    start_v += vsize_per_class
+                    end_v += vsize_per_class
+
+                train_letter = letter_set[vsize_per_class:end_l, :, :]
+                train_dataset[start_t:end_t, :, :] = train_letter
+                train_labels[start_t:end_t] = label
+                start_t += tsize_per_class
+                end_t += tsize_per_class
+        except Exception as e:
+            print('Unable to process data from', pickle_file, ':', e)
+            raise
+
+    return valid_dataset, valid_labels, train_dataset, train_labels
+
+
+train_size = 200000
+valid_size = 10000
+test_size = 10000
+
+valid_dataset, valid_labels, train_dataset, train_labels \
+    = merge_datasets(train_datasets, train_size, valid_size)
+
+_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
+
+print('Training:', train_dataset.shape, train_labels.shape)
+print('Validation:', valid_dataset.shape, valid_labels.shape)
+print('Testing:', test_dataset.shape, test_labels.shape)
+print("### End 5")
+
+# TODO:6
+print("### Start 6")
+
+
+def randomize(dataset, labels):
+    permutation = np.random.permutation(labels.shape[0])
+    shuffled_dataset = dataset[permutation, :, :]
+    shuffled_labels = labels[permutation]
+    return shuffled_dataset, shuffled_labels
+
+
+tlTmpO = 0
+for ii in range(0, train_size-1):
+    tlTmpO += abs(train_labels[ii+1] - train_labels[ii])
+
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+print("### End 6")
+print("### Problem 4")
+tlTmpR = 0
+for ii in range(0, train_size-1):
+    tlTmpR += abs(train_labels[ii+1] - train_labels[ii])
+print("tlTmpO:", tlTmpO)
+print("tlTmpR:", tlTmpR)
+
+# TODO:7
+print("### Start 7")
+pickle_file = os.path.join(data_root, 'notMNIST.pickle')
+
+try:
+    f = open(pickle_file, 'wb')
+    save = {
+        'train_dataset': train_dataset,
+        'train_labels': train_labels,
+        'valid_dataset': valid_dataset,
+        'valid_labels': valid_labels,
+        'test_dataset': test_dataset,
+        'test_labels': test_labels,
+    }
+    pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+    f.close()
+except Exception as e:
+    print('Unable to save data to', pickle_file, ':', e)
+    raise
+print("### End 7")
+
+# TODO:8
+print("### Start 8")
+statinfo = os.stat(pickle_file)
+print('Compressed pickle size:', statinfo.st_size)
+print("### End 8")
+print("### Problem 5 & 6")
 
---
+++ test02.py
@@ -0,0 +1,30 @@
+# TODO:..................................2
+print("# # # 2 # # #")
+
+# TODO:1
+print("### Start 1")
+print("### End 1")
+
+# TODO:2
+print("### Start 2")
+print("### End 2")
+
+# TODO:3
+print("### Start 3")
+print("### End 3")
+
+# TODO:4
+print("### Start 4")
+print("### End 4")
+
+# TODO:5
+print("### Start 5")
+print("### End 5")
+
+# TODO:6
+print("### Start 6")
+print("### End 6")
+
+# TODO:7
+print("### Start 7")
+print("### End 7")
